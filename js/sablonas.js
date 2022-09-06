9adc0a7c15375d3ddb49c6e11dbd7cd323a1236f
Povilas Velicka <velicka.povilas@gmail.com>
2022-09-06 16:27:16 +0300

    Delete sablonas.html

--- js/sablonas.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('button').addEventListener('click', () => buttonClicked)
-
-function buttonClicked() {
-    fetch('url', { method: 'GET' }).then(res => { return res.json() }).then(data => { return data })
-}
-
-function updateForm(data) {
-
-}
