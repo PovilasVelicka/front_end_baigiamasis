cd9366217e3f9c87895b6020d6e72802030a264e
Povilas Velicka <velicka.povilas@gmail.com>
2022-09-07 18:54:01 +0300

    updates

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 let _userInformation = null;
 const _toDoApiUrl = 'https://localhost:7171/api/ToDo'
 const _messageStyles = { complete: 'ok', information: 'info', error: 'err' }
-const selectedListType = () => sessionStorage.getItem(`${_userInformation === null ? '' : _userInformation.id}selectedListType`)
+const selectedListType = () => sessionStorage.getItem(`${_userInformation.id}selectedListType`)
 
 if (document.getElementById('logo')) document.getElementById('logo').addEventListener('click', () => document.location.href = '../index.html')
 
